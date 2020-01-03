3b9e22e07aa69cdde4dfe062967667b98752344c
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-03 17:36:04 +0100

    set-and-get-attribute.js file and style.css were updated

--- js/set-and-get-attribute.js
+++ js/set-and-get-attribute.js
@@ -1,6 +1,14 @@
-const urls = document.querySelectorAll("a");
-for (let i = 0; i < urls.length; i++){
-    urls[i].style.color = "red";
-    console.log(urls[i].getAttribute('href'));
-    urls[i].setAttribute('href','http://line-mode.cern.ch/www/hypertext/WWW/TheProject.html');
+//const urls = document.querySelectorAll("a");
+//for (let i = 0; i < urls.length; i++){
+//    urls[i].style.color = "red";
+//    console.log(urls[i].getAttribute('href'));
+//    urls[i].setAttribute('href','http://line-mode.cern.ch/www/hypertext/WWW/TheProject.html');
+//}
+
+const listItems = document.querySelectorAll("li");
+
+for (let i = 0; i < listItems.length; i++){
+    console.log(listItems[i].getAttribute('class'));
+    listItems[i].setAttribute('class','blue');
+    listItems[i].style.listStyleType = "square";
 }
