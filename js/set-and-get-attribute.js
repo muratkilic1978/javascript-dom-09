dd7d43dbd5015b0873284ab0b93b765d9eb83649
muratkilic1978 <muratkilic1978@gmail.com>
2020-01-03 22:11:33 +0100

    set-and-get-attribute.js file was updated

--- js/set-and-get-attribute.js
+++ js/set-and-get-attribute.js
@@ -2,13 +2,16 @@
 //for (let i = 0; i < urls.length; i++){
 //    urls[i].style.color = "red";
 //    console.log(urls[i].getAttribute('href'));
-//    urls[i].setAttribute('href','http://line-mode.cern.ch/www/hypertext/WWW/TheProject.html');
+//    urls[i].setAttribute('href','http://www.w3schools.com');
+//    urls[i].innerHTML = "w3schools";
 //}
 
 const listItems = document.querySelectorAll("li");
 
 for (let i = 0; i < listItems.length; i++){
-    console.log(listItems[i].getAttribute('class'));
-    listItems[i].setAttribute('class','blue');
-    listItems[i].style.listStyleType = "square";
+    console.log(listItems[i].getAttribute('class'));      if(listItems[i].classList.contains('url')){
+        listItems[i].setAttribute('class','blue');
+        listItems[i].style.listStyleType = "square";
+        
+     }
 }
